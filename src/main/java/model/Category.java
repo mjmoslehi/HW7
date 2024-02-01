7f72f8c7b14b64dd8d9b4817b555fc3236f4a8de
mamad <you@example.com>
2024-02-01 13:24:55 +0330

    add constructors

--- src/main/java/model/Category.java
+++ src/main/java/model/Category.java
@@ -4,5 +4,15 @@ public class Category {
     private int id ;
     private String name;
     private String description;
+    public Category(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
 
 }
