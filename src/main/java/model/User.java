1fa629c08444e8325de54f86d3cb7fcf3950472c
mamad <you@example.com>
2024-02-01 13:34:50 +0330

    add constructor for User class

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -6,4 +6,19 @@ public class User {
     private String userName;
     private String email;
     private String password;
+
+    public User(int id, String name, String userName, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String name, String userName, String email, String password) {
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
 }
