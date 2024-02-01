cd465bcb7e1bb94b1d72a1af9a36d871d070a6d0
mamad <you@example.com>
2024-02-01 14:03:12 +0330

    add pattern fields to Validation class

--- src/main/java/utility/Validation.java
+++ src/main/java/utility/Validation.java
@@ -1,4 +1,13 @@
 package utility;
 
+import java.util.regex.Pattern;
+
 public class Validation {
+
+    private static final Pattern USER_EMAIL_PATTERN;
+    private static final Pattern USER_PASSWORD_PATTERN;
+    private static final Pattern BRAND_WEBSITE_PATTERN;
+    private static final Pattern SHAREHOLDER_PHONE_NUMBER_PATTERN;
+    private static final Pattern SHAREHOLDER_NATIONAL_CODE_PATTERN;
+
 }
