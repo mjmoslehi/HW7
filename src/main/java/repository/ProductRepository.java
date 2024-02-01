25f8f53dc471d9051cfae0dc6868ead0b2f42e50
mamad <you@example.com>
2024-02-01 13:52:28 +0330

    add fields and constructor for ProductRepository class

--- src/main/java/repository/ProductRepository.java
+++ src/main/java/repository/ProductRepository.java
@@ -1,4 +1,11 @@
 package repository;
 
+import java.sql.Connection;
+
 public class ProductRepository {
+    private final Connection connection;
+
+    public ProductRepository(Connection connection) {
+        this.connection = connection;
+    }
 }
