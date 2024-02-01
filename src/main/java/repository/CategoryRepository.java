d536a196c597fc10165e44d101cbc405eaaca161
mamad <you@example.com>
2024-02-01 13:50:25 +0330

    add loadAllCategory method for CategoryRepository class

--- src/main/java/repository/CategoryRepository.java
+++ src/main/java/repository/CategoryRepository.java
@@ -4,6 +4,7 @@ import model.Category;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CategoryRepository {
@@ -20,6 +21,36 @@ public class CategoryRepository {
         preparedStatement.setString(1, category.getName());
         preparedStatement.setString(2, category.getDescription());
         return preparedStatement.executeUpdate();
+    }
+
+    public Category[] loadAllCategory() throws SQLException {
+
+        String loadCategories = "SELECT * FROM category ";
+        PreparedStatement preparedStatement = connection.prepareStatement(loadCategories ,
+                ResultSet.TYPE_SCROLL_SENSITIVE,
+                ResultSet.TYPE_SCROLL_INSENSITIVE);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        int count = 0;
+
+        while (resultSet.next()) {
+            count++;
+        }
+
+        Category[] categories = new Category[count];
+
+        int i = 0;
+        resultSet.beforeFirst();
+
+        while (resultSet.next()) {
+
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            String description = resultSet.getString("description");
+            categories[i] = new Category(id, name, description);
+            System.out.println(categories[i]);
+            i++;
+        }
+        return categories;
     }
 
 }
