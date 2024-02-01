0179ba440a815b32056ea1cf4dbe10bc61cd6fce
mamad <you@example.com>
2024-02-01 14:14:55 +0330

    add fields and constructor for BrandService class

--- src/main/java/service/BrandService.java
+++ src/main/java/service/BrandService.java
@@ -1,4 +1,14 @@
 package service;
 
+import repository.BrandRepository;
+
+import java.util.Scanner;
+
 public class BrandService {
+    private final Scanner scanner = new Scanner(System.in);
+    private final BrandRepository brandRepository;
+
+    public BrandService(BrandRepository brandRepository) {
+        this.brandRepository = brandRepository;
+    }
 }
