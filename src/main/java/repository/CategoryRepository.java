ac855e4b918ee3fd0e3f5c4b7726af4ecfce59a4
mamad <you@example.com>
2024-02-01 13:49:56 +0330

    add register method for CategoryRepository class

--- src/main/java/repository/CategoryRepository.java
+++ src/main/java/repository/CategoryRepository.java
@@ -1,6 +1,10 @@
 package repository;
 
+import model.Category;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class CategoryRepository {
     private final Connection connection;
@@ -8,4 +12,14 @@ public class CategoryRepository {
     public CategoryRepository(Connection connection) {
         this.connection = connection;
     }
+
+    public int register(Category category) throws SQLException {
+
+        String registerCategory = "INSERT INTO category (name, description) VALUES (?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(registerCategory);
+        preparedStatement.setString(1, category.getName());
+        preparedStatement.setString(2, category.getDescription());
+        return preparedStatement.executeUpdate();
+    }
+
 }
