825bf52734fa3993cdb2f0dad1620a8f22e45c28
mamad <you@example.com>
2024-02-01 14:26:58 +0330

    add things to Shareholder_brandService class

--- src/main/java/service/Shareholder_brandService.java
+++ src/main/java/service/Shareholder_brandService.java
@@ -1,4 +1,28 @@
 package service;
 
+import model.Shareholder_Brand;
+import repository.Shareholder_brandRepository;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Shareholder_brandService {
+    private final Scanner scanner = new Scanner(System.in);
+    private final Shareholder_brandRepository shareholderBrandRepository ;
+
+    public Shareholder_brandService(Shareholder_brandRepository shareholderBrandRepository) {
+        this.shareholderBrandRepository = shareholderBrandRepository;
+    }
+
+    public void register()throws SQLException {
+        System.out.println("enter shareholder id :");
+        int shareholderId = scanner.nextInt();
+
+        System.out.println("enter brand id :");
+        int brandId = scanner.nextInt();
+
+        Shareholder_Brand shareholderBrand = new Shareholder_Brand(shareholderId,brandId);
+        shareholderBrandRepository.register(shareholderBrand);
+        System.out.println(shareholderBrand);
+    }
 }
