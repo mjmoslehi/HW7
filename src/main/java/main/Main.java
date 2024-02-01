b049acc89d4058296c6f083c8534281abf32f808
mamad <you@example.com>
2024-02-01 14:26:11 +0330

    add things to Main class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,4 +1,13 @@
 package main;
 
+import menu.Menu;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+
 public class Main {
+    public static void main(String[] args) throws SQLException, ParseException {
+        Menu menu = new Menu();
+        menu.publicMenu();
+    }
 }
