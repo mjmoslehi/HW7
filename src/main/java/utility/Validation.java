ff6d85974b5def6839d2f13cb2aa28425f357b7c
mamad <you@example.com>
2024-02-01 14:03:44 +0330

    add regexes to Validation class

--- src/main/java/utility/Validation.java
+++ src/main/java/utility/Validation.java
@@ -10,4 +10,15 @@ public class Validation {
     private static final Pattern SHAREHOLDER_PHONE_NUMBER_PATTERN;
     private static final Pattern SHAREHOLDER_NATIONAL_CODE_PATTERN;
 
+    static {
+        USER_EMAIL_PATTERN =Pattern.compile("^[A-Z0-9_!#$%&'*+/=?`{|}~^-]" +
+                "+(?:\\.[A-Z0-9_!#$%&'*+/=?`{|}~^-]+↵\n" + ")*@[A-Z0-9-]+(?:\\.[A-Z0-9-]+)*$",Pattern.CASE_INSENSITIVE);
+        USER_PASSWORD_PATTERN =Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#!%&*])" +
+                "[A-Za-z0-9@#!%&*]{8,10}$");
+        BRAND_WEBSITE_PATTERN = Pattern.compile("<\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]" +
+                "*[-a-zA-Z0-9+&@#/%=~_|]>");
+        SHAREHOLDER_PHONE_NUMBER_PATTERN =Pattern.compile("(0|\\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]" +
+                "([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}");
+        SHAREHOLDER_NATIONAL_CODE_PATTERN =Pattern.compile("[0-9]{10}");
+    }
 }
