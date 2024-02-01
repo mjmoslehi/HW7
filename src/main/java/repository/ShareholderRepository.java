108e6f908b422b3ffde09380f7cf713b24739d72
mamad <you@example.com>
2024-02-01 13:56:11 +0330

    add field and constructor for ShareholderRepository class

--- src/main/java/repository/ShareholderRepository.java
+++ src/main/java/repository/ShareholderRepository.java
@@ -1,4 +1,12 @@
 package repository;
 
+import java.sql.Connection;
+
 public class ShareholderRepository {
+
+    private final Connection connection;
+
+    public ShareholderRepository(Connection connection) {
+        this.connection = connection;
+    }
 }
