6c3f454154b1c4afedbdb143fb4ebf99a37d75f8
mamad <you@example.com>
2024-02-01 13:23:07 +0330

    add constructors

--- src/main/java/model/Brand.java
+++ src/main/java/model/Brand.java
@@ -5,4 +5,17 @@ public class Brand {
     private String name ;
     private String website;
     private String description;
+
+    public Brand(String name, String website, String description) {
+        this.name = name;
+        this.website = website;
+        this.description = description;
+    }
+
+    public Brand(int id , String name, String website, String description) {
+        this.id =id;
+        this.name = name;
+        this.website = website;
+        this.description = description;
+    }
 }
