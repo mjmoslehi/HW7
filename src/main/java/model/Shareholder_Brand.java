125bb8c19733262b4a99600cfe0fb156c70ed477
mamad <you@example.com>
2024-02-01 13:33:11 +0330

    add constructor for Shareholder_Brand class

--- src/main/java/model/Shareholder_Brand.java
+++ src/main/java/model/Shareholder_Brand.java
@@ -1,6 +1,12 @@
 package model;
 
 public class Shareholder_Brand {
+
     private int idShareholder;
     private int idBrand;
+
+    public Shareholder_Brand(int idShareholder, int idBrand) {
+        this.idShareholder = idShareholder;
+        this.idBrand = idBrand;
+    }
 }
