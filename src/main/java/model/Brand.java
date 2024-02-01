6466e961827c46fc81874b973610636cf99aa609
mamad <you@example.com>
2024-02-01 13:23:43 +0330

    add setter and getter and toString method

--- src/main/java/model/Brand.java
+++ src/main/java/model/Brand.java
@@ -18,4 +18,45 @@ public class Brand {
         this.website = website;
         this.description = description;
     }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", website='" + website + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
