e0ac465e43db8813d4970f5648ba158af11b55b9
mamad <you@example.com>
2024-02-01 23:56:39 +0330

    add load method for Shareholder_brandRepository class

--- src/main/java/repository/Shareholder_brandRepository.java
+++ src/main/java/repository/Shareholder_brandRepository.java
@@ -4,6 +4,7 @@ import model.Shareholder_Brand;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Shareholder_brandRepository {
@@ -27,5 +28,22 @@ public class Shareholder_brandRepository {
         preparedStatement.setInt(1,shareholderBrand.getId());
         preparedStatement.executeUpdate();
         return shareholderBrand;
+    }
+
+    public Shareholder_Brand load (int shBrId) throws SQLException{
+
+        String loadShBr = "SELECT * FROM shareholder_brand WHERE id =?";
+        PreparedStatement preparedStatement = connection.prepareStatement(loadShBr );
+        preparedStatement.setInt(1,shBrId);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (resultSet.next()){
+            int id = resultSet.getInt("id");
+            int idShareholder = resultSet.getInt("id_shareholder");
+            int idBrand = resultSet.getInt("id_brand");
+            Shareholder_Brand shareholderBrand = new Shareholder_Brand(id , idShareholder ,idBrand);
+            return shareholderBrand;
+        }else return null;
+
     }
 }
