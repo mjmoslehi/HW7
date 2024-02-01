950fe08aa3dc18d6b5459d131874c953b0215750
mamad <you@example.com>
2024-02-01 13:38:30 +0330

    create body of JdbcConnection class

--- src/main/java/connection/JdbcConnection.java
+++ src/main/java/connection/JdbcConnection.java
@@ -1,4 +1,26 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+
+    private static final Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+                    "postgres", "mjm6yhn7ujm");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public JdbcConnection() {
+    }
+
+    static public Connection getConnection() {
+        return connection;
+    }
 }
