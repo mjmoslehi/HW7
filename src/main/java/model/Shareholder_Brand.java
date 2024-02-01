567d73590acbfe0f28388aaf192dcfa25b17949d
mamad <you@example.com>
2024-02-01 13:33:47 +0330

    add getter and setter and toString methods for Shareholder_Brand class

--- src/main/java/model/Shareholder_Brand.java
+++ src/main/java/model/Shareholder_Brand.java
@@ -9,4 +9,29 @@ public class Shareholder_Brand {
         this.idShareholder = idShareholder;
         this.idBrand = idBrand;
     }
+
+
+    public int getIdShareholder() {
+        return idShareholder;
+    }
+
+    public void setIdShareholder(int idShareholder) {
+        this.idShareholder = idShareholder;
+    }
+
+    public int getIdBrand() {
+        return idBrand;
+    }
+
+    public void setIdBrand(int idBrand) {
+        this.idBrand = idBrand;
+    }
+
+    @Override
+    public String toString() {
+        return "Shareholder_Brand{" +
+                "idShareholder=" + idShareholder +
+                ", idBrand=" + idBrand +
+                '}';
+    }
 }
