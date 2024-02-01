f1aacebd8ceb125e49520eca91cfc9ea9575ee05
mamad <you@example.com>
2024-02-01 13:30:37 +0330

    add setter and getter and toString methods for Shareholder class

--- src/main/java/model/Shareholder.java
+++ src/main/java/model/Shareholder.java
@@ -18,4 +18,47 @@ public class Shareholder {
         this.nationalCode = nationalCode;
     }
 
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Shareholder{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", nationalCode='" + nationalCode + '\'' +
+                '}';
+    }
+
 }
