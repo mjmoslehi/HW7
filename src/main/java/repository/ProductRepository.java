7ee0e98402217d16fb9147396a3adb87717cc9d6
mamad <you@example.com>
2024-02-01 13:52:56 +0330

    add register method for ProductRepository class

--- src/main/java/repository/ProductRepository.java
+++ src/main/java/repository/ProductRepository.java
@@ -1,11 +1,25 @@
 package repository;
 
+import model.Product;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class ProductRepository {
     private final Connection connection;
 
     public ProductRepository(Connection connection) {
         this.connection = connection;
+    }
+
+    public int register(Product product)throws SQLException {
+        String registerProduct = "INSERT INTO product (name, create_date, category_id, brand_id) VALUES (?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(registerProduct);
+        preparedStatement.setString(1,product.getName());
+        preparedStatement.setDate(2,product.getCreateDate());
+        preparedStatement.setInt(3,product.getCategoryId());
+        preparedStatement.setInt(4,product.getBrandId());
+        return preparedStatement.executeUpdate();
     }
 }
