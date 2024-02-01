8b11d3d4307e45d8f06cf1efa7f69b39d62674b0
mamad <you@example.com>
2024-02-01 13:39:56 +0330

    add registerBrand method

--- src/main/java/repository/BrandRepository.java
+++ src/main/java/repository/BrandRepository.java
@@ -1,11 +1,24 @@
 package repository;
 
+import model.Brand;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class BrandRepository {
     private final Connection connection;
 
     public BrandRepository(Connection connection) {
         this.connection = connection;
+    }
+
+    public int registerBrand(Brand brand) throws SQLException {
+        String addBrand = "INSERT INTO brand (name, website, description) VALUES (?,?,?);";
+        PreparedStatement preparedStatement = connection.prepareStatement(addBrand);
+        preparedStatement.setString(1, brand.getName());
+        preparedStatement.setString(2, brand.getWebsite());
+        preparedStatement.setString(3, brand.getDescription());
+        return preparedStatement.executeUpdate();
     }
 }
