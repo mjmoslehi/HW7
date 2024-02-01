dd600bb58fd2947f16493ab488e7acec0cd76382
mamad <you@example.com>
2024-02-01 14:20:19 +0330

    add fields and constructor for BrandRepository class

--- src/main/java/service/CategoryService.java
+++ src/main/java/service/CategoryService.java
@@ -1,4 +1,14 @@
 package service;
 
+import repository.CategoryRepository;
+
+import java.util.Scanner;
+
 public class CategoryService {
+    private final Scanner scanner = new Scanner(System.in);
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 }
