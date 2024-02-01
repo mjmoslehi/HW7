a3cfc9b1684c12d9739ff91953b8e3f9a988fca0
mamad <you@example.com>
2024-02-01 14:05:12 +0330

    add static fields to ApplicationContext class

--- src/main/java/utility/ApplicationContext.java
+++ src/main/java/utility/ApplicationContext.java
@@ -1,4 +1,24 @@
 package utility;
 
+import repository.*;
+import service.*;
+
+import java.sql.Connection;
+
 public class ApplicationContext {
+    private static final Connection CONNECTION;
+
+    private static final BrandRepository BRAND_REPOSITORY;
+    private static final CategoryRepository CATEGORY_REPOSITORY;
+    private static final ProductRepository PRODUCT_REPOSITORY;
+    private static final Shareholder_brandRepository SHAREHOLDER_BRAND_REPOSITORY;
+    private static final ShareholderRepository SHAREHOLDER_REPOSITORY;
+    private static final UserRepository USER_REPOSITORY;
+
+    private static final BrandService BRAND_SERVICE;
+    private static final CategoryService CATEGORY_SERVICE;
+    private static final ProductService PRODUCT_SERVICE;
+    private static final Shareholder_brandService SHAREHOLDER_BRAND_SERVICE;
+    private static final ShareholderService SHAREHOLDER_SERVICE;
+    private static final UserService USER_SERVICE;
 }
