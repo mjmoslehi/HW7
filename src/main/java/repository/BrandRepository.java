d3379fa976c1d5fb9df57fcee4f3781e9e0bb0cf
mamad <you@example.com>
2024-02-01 13:39:29 +0330

    add connection field and its constructor

--- src/main/java/repository/BrandRepository.java
+++ src/main/java/repository/BrandRepository.java
@@ -1,4 +1,11 @@
 package repository;
 
+import java.sql.Connection;
+
 public class BrandRepository {
+    private final Connection connection;
+
+    public BrandRepository(Connection connection) {
+        this.connection = connection;
+    }
 }
