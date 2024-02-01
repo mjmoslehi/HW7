de6c1ea6aa01f30967a4cba1afc1dd15f9d335a5
mamad <you@example.com>
2024-02-01 13:45:00 +0330

    add loadAllBrand method

--- src/main/java/repository/BrandRepository.java
+++ src/main/java/repository/BrandRepository.java
@@ -4,6 +4,7 @@ import model.Brand;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BrandRepository {
@@ -20,5 +21,36 @@ public class BrandRepository {
         preparedStatement.setString(2, brand.getWebsite());
         preparedStatement.setString(3, brand.getDescription());
         return preparedStatement.executeUpdate();
+    }
+
+    public Brand[] loadAllBrand() throws SQLException {
+        String loadBrands = "SELECT * FROM brand ";
+        PreparedStatement preparedStatement = connection.prepareStatement(loadBrands,
+                ResultSet.TYPE_SCROLL_SENSITIVE ,
+                ResultSet.TYPE_SCROLL_INSENSITIVE);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        int count = 0;
+        while (resultSet.next()) {
+            count++;
+        }
+
+        Brand[] brands = new Brand[count];
+        int i = 0;
+
+        resultSet.beforeFirst();
+
+        while (resultSet.next()) {
+
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            String website = resultSet.getString("website");
+            String description = resultSet.getString("description");
+
+            brands[i] = new Brand(id, name, website, description);
+            System.out.println(brands[i]);
+            i++;
+        }
+        return brands;
     }
 }
