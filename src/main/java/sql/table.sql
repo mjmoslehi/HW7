ac7a663df209f3926f72295fb978085f27811fb8
mamad <you@example.com>
2024-02-01 13:42:27 +0330

    create category table

--- src/main/java/sql/table.sql
+++ src/main/java/sql/table.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS shareholder
     phone_number  varchar(11),
     national_code varchar(10) unique
 );
+
 CREATE table if not exists brand
 (
     id          serial primary key,
@@ -12,10 +13,18 @@ CREATE table if not exists brand
     website     varchar,
     description varchar
 );
+
 create table if not exists shareholder_brand
 (
     id_shareholder integer,
     id_brand       integer,
     foreign key (id_shareholder) references shareholder (id),
     foreign key (id_brand) references brand (id)
+);
+
+create table if not exists category
+(
+    id          serial primary key,
+    name        varchar unique ,
+    description varchar
 );
