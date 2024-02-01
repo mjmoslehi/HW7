a069a0078f959a5bf2df17c1fc37e346d0e42f9c
mamad <you@example.com>
2024-02-01 13:55:27 +0330

    add register method for Shareholder_brandRepository class

--- src/main/java/repository/Shareholder_brandRepository.java
+++ src/main/java/repository/Shareholder_brandRepository.java
@@ -1,11 +1,23 @@
 package repository;
 
+import model.Shareholder_Brand;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Shareholder_brandRepository {
     private final Connection connection;
 
     public Shareholder_brandRepository(Connection connection) {
         this.connection = connection;
+    }
+
+    public int register(Shareholder_Brand shareholderBrand) throws SQLException {
+        String registerShareholderBrand = "INSERT INTO shareholder_brand VALUES (?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(registerShareholderBrand);
+        preparedStatement.setInt(1,shareholderBrand.getIdShareholder());
+        preparedStatement.setInt(2,shareholderBrand.getIdBrand());
+        return preparedStatement.executeUpdate();
     }
 }
