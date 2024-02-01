e4b18bea086e09f84e68b55793ba8b382a1c595f
mamad <you@example.com>
2024-02-01 13:49:22 +0330

    add field and constructor for CategoryRepository class

--- src/main/java/repository/CategoryRepository.java
+++ src/main/java/repository/CategoryRepository.java
@@ -1,4 +1,11 @@
 package repository;
 
+import java.sql.Connection;
+
 public class CategoryRepository {
+    private final Connection connection;
+
+    public CategoryRepository(Connection connection) {
+        this.connection = connection;
+    }
 }
