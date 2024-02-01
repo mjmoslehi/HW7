bdc81a56e98dd94b78b8a589184085de70ab4d38
mamad <you@example.com>
2024-02-01 13:27:27 +0330

    add fields

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,4 +1,11 @@
 package model;
 
+import java.sql.Date;
+
 public class Product {
+    private int id ;
+    private String name;
+    private Date createDate;
+    private int categoryId;
+    private int brandId;
 }
