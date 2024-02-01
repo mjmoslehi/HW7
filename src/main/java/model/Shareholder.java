561b37e7738d7af06d14d6d26a263d3e03efbdec
mamad <you@example.com>
2024-02-01 13:29:57 +0330

    add constructor for Shareholder class

--- src/main/java/model/Shareholder.java
+++ src/main/java/model/Shareholder.java
@@ -5,4 +5,17 @@ public class Shareholder {
     private String name;
     private String phoneNumber;
     private String nationalCode;
+    public Shareholder(int id, String name, String phoneNumber, String nationalCode) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.nationalCode = nationalCode;
+    }
+
+    public Shareholder(String name, String phoneNumber, String nationalCode) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.nationalCode = nationalCode;
+    }
+
 }
