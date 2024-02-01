38210529f15fe87c957fb59ee6e5d2cb3c33a886
mamad <you@example.com>
2024-02-01 13:27:49 +0330

    add constructors

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -8,4 +8,19 @@ public class Product {
     private Date createDate;
     private int categoryId;
     private int brandId;
+
+    public Product(int id, String name, Date createDate, int categoryId, int brandId) {
+        this.id = id;
+        this.name = name;
+        this.createDate = createDate;
+        this.categoryId = categoryId;
+        this.brandId = brandId;
+    }
+
+    public Product(String name, Date createDate, int categoryId, int brandId) {
+        this.name = name;
+        this.createDate = createDate;
+        this.categoryId = categoryId;
+        this.brandId = brandId;
+    }
 }
